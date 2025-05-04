4d9cc1fb04d3d338347fe2b558124d1eb1b1d8b5
ejkmvp@umsystem.edu <ejkmvp@umsystem.edu>
2025-05-03 20:38:25 -0500

    work on getting buttons implemented in the settings

--- kit.tv/app/(dashboard)/u/[username]/chat/page.tsx
+++ kit.tv/app/(dashboard)/u/[username]/chat/page.tsx
@@ -1,37 +1,50 @@
 import { getSelf } from "@/lib/auth-service";
 import { getStreamByUserId } from "@/lib/stream-service";
 import { ToggleCard } from "./_components/toggle-card";
+import { getButtons } from "@/lib/button-service";
+import { Button } from "@prisma/client";
+import { CreateButtonDialog } from "./_components/create-button-dialog";
 
 const ChatPage = async () => {
   const self = await getSelf();
   const stream = await getStreamByUserId(self.id);
+  const buttons = await getButtons(self.id);
 
   if (!stream) {
     throw new Error("Stream not found");
   }
 
   return (
-    <div className="p-6">
-      <div className=" mb-4">
-        <h1 className=" text-2xl font-bold">Chat Settings</h1>
+    <div className="p-6 space-y-8">
+      <div>
+        <h1 className="text-2xl font-bold mb-4">Chat Settings</h1>
+        <div className="space-y-4">
+          <ToggleCard
+            field="isChatEnabled"
+            label="Enable chat"
+            value={stream.isChatEnabled}
+          />
+          <ToggleCard
+            field="isChatDelayed"
+            label="Delay chat"
+            value={stream.isChatDelayed}
+          />
+          <ToggleCard
+            field="isChatFollowersOnly"
+            label="Must be following to chat"
+            value={stream.isChatFollowersOnly}
+          />
+        </div>
       </div>
 
-      <div className="space-y-4">
-        <ToggleCard
-          field="isChatEnabled"
-          label="Enable chat"
-          value={stream.isChatEnabled}
-        />
-        <ToggleCard
-          field="isChatDelayed"
-          label="Delay chat"
-          value={stream.isChatDelayed}
-        />
-        <ToggleCard
-          field="isChatFollowersOnly"
-          label="Must be following to chat"
-          value={stream.isChatFollowersOnly}
-        />
+      <div>
+        <h1 className="text-2xl font-bold mb-4">Buttons</h1>
+        <div className="space-y-4">
+          {buttons.map((button:Button) => (
+            <div key={button.id}>{button.id}</div>
+          ))}
+          <CreateButtonDialog streamerId={self.id} />
+        </div>
       </div>
     </div>
   );
