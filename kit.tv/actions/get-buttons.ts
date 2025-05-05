6d7515517b45a4b94aa9761ffb29f9d556711338
ejkmvp@umsystem.edu <ejkmvp@umsystem.edu>
2025-05-05 04:40:52 -0500

    worked more on chat buttons, not fully finished yet

--- kit.tv/actions/get-buttons.ts
+++ kit.tv/actions/get-buttons.ts
@@ -1,5 +1,6 @@
 "use server";
 
+import { pressButton } from "@/lib/button-queue-service";
 import { getButtons } from "@/lib/button-service";
 import { revalidatePath } from "next/cache";
 
@@ -10,7 +11,22 @@ export const getButtonsAction = async (streamUserId: string) => {
         return { success: true, buttons: buttons };
     } catch (error) {
         console.error("Error fetching buttons:", error);
+        revalidatePath("/"); 
         return { success: false, error: "Error fetching buttons" };
     }
 };
+
+export const pressButtonAction = async (buttonId: string) => {
+    try {
+        const button = await pressButton(buttonId);
+        revalidatePath("/"); 
+        return { success: true, button: button };
+    } catch (error) {
+        console.error("Error pressing button:", error);
+        revalidatePath("/"); 
+        return { success: false, error: "Error pressing button" };
+    }
+};
+
+
 
