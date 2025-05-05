492a94c8f62fab2408314eb3c6c3cf07906be42e
Caleb Robinson <cmrd2d@umsystem.edu>
2025-05-05 14:01:09 -0500

    moRE Colors

--- kit.tv/app/(browse)/_components/sidebar/index.tsx
+++ kit.tv/app/(browse)/_components/sidebar/index.tsx
@@ -12,7 +12,7 @@ export const Sidebar = async () => {
   return (
     <Wrapper>
       <Toggle />
-      <div className="space-y-4 pt-4 lg:pt-0 bg-[#2B1605]">
+      <div className="space-y-4 pt-4 lg:pt-0 bg-[#0B0804]">
         <Following data={following} />
         <Recommended data={recommended} />
       </div>
@@ -22,7 +22,7 @@ export const Sidebar = async () => {
 
 export const SidebarSkeleton = () => {
   return (
-    <aside className="fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2D2E35] z-50">
+    <aside className="fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#0B0804] z-50">
       <ToggleSkeleton />
       <FollowingSkeleton />
       <RecommendedSkeleton />
